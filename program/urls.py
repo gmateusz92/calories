580f53304c8955ed2f3725af3c42c33f006f7b33
mateusz <matek1600@o2.pl>
2022-11-17 15:09:16 +0100

    1

--- program/urls.py
+++ program/urls.py
@@ -4,5 +4,6 @@ from program import views
 urlpatterns = [
 
     path('',views.index,name="index"),
+    path('count', views.count, name='count'),
     path('delete/<int:id>/', views.delete_consume, name="delete"),
 ]
--- program/views.py
+++ program/views.py
@@ -11,10 +11,73 @@ def index(request):
         consume = Consume(user=user, food_consumed=consume)#tworze obiekt dla klasy Consume
         consume.save()
         foods = Food.objects.all()
+
+        # consumed = Consume.objects.all()
+        #
+        # cal_querysetConsumed = []
+        # for item in consumed:
+        #     cal_querysetConsumed.append(item.food_consumed.calories)
+        # qsc = cal_querysetConsumed
+        # total = 0
+        # for item in qsc:
+        #     total = item + total
+
+        # carb_querysetConsumed = []
+        # for item in consumed:
+        #     carb_querysetConsumed.append(item.food_consumed.carbs)
+        # qsc = carb_querysetConsumed
+        # carb_total = 0
+        # for item in qsc:
+        #     carb_total = item + carb_total
+
     else:
         foods = Food.objects.all()
+
+        consumed = Consume.objects.all()
+        cal_querysetConsumed = []
+        for item in consumed:
+            cal_querysetConsumed.append(item.food_consumed.calories)
+        qsc = cal_querysetConsumed
+        total = 0
+        for item in qsc:
+            total = item + total
+
+        carb_querysetConsumed = []
+        for item in consumed:
+            carb_querysetConsumed.append(item.food_consumed.carbs)
+        qsc = carb_querysetConsumed
+        carb_total = 0
+        for item in qsc:
+            carb_total = item + carb_total
+
     consumed_food = Consume.objects.filter(user=request.user)
-    return render(request, 'index.html', {'foods': foods, 'consumed_food': consumed_food})
+    return render(request, 'index.html',  {'foods': foods, 'consumed_food': consumed_food, 'total': total, 'carb_total': carb_total})
+
+def count(request,):
+    user = request.user
+    food_item = Food.objects.all()
+    consumed = Consume.objects.all()
+    querysetFood=[]
+    for food in food_item:
+        querysetFood.append(food.calories)
+    qs = querysetFood
+    querysetConsumed = []
+    for item in consumed:
+        querysetConsumed.append(item.food_consumed.calories)
+    qsc = querysetConsumed
+    total = 0
+    for item in qsc:
+        total = item + total
+    context = {
+        'user': user,
+        'food_item': food_item,
+        'consumed': consumed,
+        'qs': qs,
+        'qsc': qsc,
+        'total': total,
+    }
+    return render(request, 'count.html', context)
+
 
 
 def delete_consume(request, id):
@@ -23,3 +86,5 @@ def delete_consume(request, id):
         consumed_food.delete()
         return redirect('/')
     return render(request, 'delete.html')
+
+
